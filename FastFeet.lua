181d8c53e86c04c1aa4399ffa46945d0ec3077a6
Gonzalo Carrillo <mynameisgonz@gmail.com>
2020-06-23 19:50:33 -0400

    Update FastFeet.lua
    added combat toggle and fixed some bugs

--- FastFeet.lua
+++ FastFeet.lua
@@ -43,10 +43,10 @@ windower.register_event('addon command', function(...)
 	local cmd = table.remove(args,1):lower()
 	
 	if args ~= nil then
-		if cmd == 'set' then
-			-- the latest information about the character
-			local p = windower.ffxi.get_player()
-				
+		-- the latest information about the character
+		local p = windower.ffxi.get_player()
+			
+		if cmd == 'set' then				
 			-- does the xml node for the character exist? if not, create
 			if settings.characters[string.lower(p.name)] == nil then
 				settings.characters[string.lower(p.name)] = {}
@@ -59,14 +59,30 @@ windower.register_event('addon command', function(...)
 				settings:save('all')
 			end
 			
+			-- does the xml node for the status toggle exist? if not, create
+			if settings.characters[string.lower(p.name)][string.lower(p.main_job)].engaged == nil then
+				settings.characters[string.lower(p.name)][string.lower(p.main_job)].engaged = true
+				settings:save('all')
+			end
+			
 			if args[1] ~= nil or args[1] ~= "" then
 				if search_item(args[1]) then
 					settings.characters[string.lower(p.name)][string.lower(p.main_job)].item = args[1]
-					log('Fast Item set to ' .. me.fast_item.name .. ' for ' .. p.name .. ' on ' .. p.main_job)
+					log('Fast Item set to ' .. args[1] .. ' for ' .. p.name .. ' on ' .. p.main_job)
 					settings:save('all')
 				else
 					log('could not find item')
 				end
+			end
+		elseif cmd == 'engaged' then
+			if settings.characters[string.lower(p.name)][string.lower(p.main_job)].engaged then
+				settings.characters[string.lower(p.name)][string.lower(p.main_job)].engaged = false
+				log('Item swap in combat set to false')
+				settings:save('all')
+			else
+				settings.characters[string.lower(p.name)][string.lower(p.main_job)].engaged = true
+				log('Item swap in combat set to true')
+				settings:save('all')
 			end
 		end
 	end
@@ -80,24 +96,36 @@ function update()
 		me:update(windower.ffxi.get_mob_by_target('me'))
 		
 		if check_fast_item() then
-			if me:moving() then
+			if me:moving() and checkCombat() then
 				--me:equip_item(me.fast_item)
 				local equipped = me:get_currently_equipped_item()
 				
 				--save the previously equipped item
 				check_current_equip_item(equipped)
 				
-				if equipped.en ~= me.fast_item.name then
+				if equipped and equipped.en ~= me.fast_item.name then
 					me:equip_item(me.fast_item)
 				end
 			else
 				local equipped = me:get_currently_equipped_item()
 				
-				if me.last_equipped ~= nil and equipped.en ~= me.last_equipped.name then
+				if equipped ~= nil and me.last_equipped ~= nil and equipped.en ~= me.last_equipped.name then
 					me:equip_item(me.last_equipped)
 				end
 			end
 		end
+	end
+end
+
+function checkCombat()
+	if settings.characters[string.lower(me.name)][string.lower(me.main_job)].engaged then
+		if me.status == 1 then
+			return false
+		else
+			return true
+		end
+	else
+		return true
 	end
 end
 
@@ -205,7 +233,7 @@ function search_item(armor)
 end
 
 function check_current_equip_item(armor)
-	if armor.en == me.fast_item.name then return end
+	if armor and armor.en == me.fast_item.name then return end
 	
 	if armor ~= nil then
 		for index,i in ipairs(windower.ffxi.get_items('inventory')) do
